719a39c8a60abfd06bd9d424bcd4e770a22b5e36
Maci Slenes <maci.slenes@gmail.com>
2019-09-22 00:08:24 -0700

    finish commenting out the code

--- app/public/app.js
+++ app/public/app.js
@@ -1,35 +1,47 @@
 
 
+// grabbing our HTML element with the id 'btn'
 const button = $('.btn');
 
 
 
+// .on('click) function that runs our api request to post the information to our server
 button.on('click', e => {
 
+    // prevent the submit button click from refreshing the page
     e.preventDefault();
+    // storing our input values in variables
     let name = $('#name').val();
     let photo = $('#photo').val();
     let answerResponse = [];
+
+    // for() to iterate through our response and grab the values of each of the questions
     for(let i = 1; i <= 10; i++ ){
 
+        // the syntax we use to get the response from the 'radio' buttons
         let selector = 'input:radio[name=q' + i + ']:checked';
 
+        // pusing the response answers into the array by using parseInt to return them as a numerical value
         answerResponse.push(parseInt($(selector).val()));
 
     };
 
+    // our key value pairs assigning 
     postRequestData = {
         name: name,
         photo: photo,
         answers: answerResponse
     }
+
     console.log(postRequestData);
 
+    // api call to add the new entry into our friends list
     $.ajax("/api/friends", {
         type: "POST",
         data: postRequestData
       }).done(function(data){
 
+        // one this request is finished we will return our response from the friends.js file and set our modal elements to reflect the new match
         $('#matchName').text(data.name);
         $('#matchPhoto').attr('src', data.photo);
         $('#matchModal').modal('show');
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,9 +16,9 @@ module.exports = function(app) {
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
-
   app.get("/api/friends", function(req, res) {
 
+    // we respond with our friends list in JSON format
     res.json(friendsData);
 
   });
@@ -32,54 +32,71 @@ module.exports = function(app) {
   // (ex. User fills out a reservation request... this data is then sent to the server...
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
-
   app.post("/api/friends", function(req, res) {
-
+  
+    // a vairable to store the  parsed response array
     let intAnswers = [];
 
     req.body.answers.forEach(element => {
       intAnswers.push(parseInt(element));
     });
     
+    // assigning req.body.answers the new answers in integers 
     req.body.answers = intAnswers;
 
+    // callback function comparing the first person on file with the new users input
     let calculateDifference = (arrOne, arrTwo) => {
 
+      // variable to track the difference between the strings
       let totalDifference = 0;
 
+      // for() to iterate through the first array
       for(let i = 0; i < arrOne.length; i++){
 
+        // using Math.abs to return the difference between the two arrays as they iterate through the for() to return a whole number
         let difference = Math.abs(arrOne[i] - arrTwo[i]);
 
+        // the total difference will now be set the the difference we just calculated
         totalDifference += difference;
 
       };
 
+      // return the total 
       return totalDifference;
 
     };
 
+    // set a min difference to compare the current difference against
     let minDiff = 9999;
     let minDiffIndex = -1;
+    // our counter as we iterate through the array
     let index = 0;
 
 
+    // taking our friends data and iterating through the array
     friendsData.forEach(person => {
 
+      // run our function to calculate the differnce on the persons answers we are retreving from the database to the ones the user just input
       let currentDifference = calculateDifference(person.answers, req.body.answers);
 
+      // if the difference between the two is less than the min difference
       if(currentDifference < minDiff){
 
+        // we set a new minDiff
         minDiff = currentDifference;
+        // change our index to reflect the winner at the end
         minDiffIndex = index;
 
       };
 
+      // if this is not met we add to the index and iterate through the friends array again
       index++;
     });
     
+    // push our user input into the friendsData
     friendsData.push(req.body);
     console.log(friendsData[minDiffIndex]);
+    // send a res of our friend data to the index of the best matched person
     res.json(friendsData[minDiffIndex]);
 
   });
